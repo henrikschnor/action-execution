ef5ee81edbd5376d778ba7b8b8f786603c3e6242
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-04-01 15:45:29 +0200

    [ae_ros_message_converter, converter_base] Removed the abstractmethod decorator

--- action_execution_ros/ae_ros_message_converter/ros/src/ae_ros_message_converter/converters/converter_base.py
+++ action_execution_ros/ae_ros_message_converter/ros/src/ae_ros_message_converter/converters/converter_base.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
-import abc
-
 class ConverterBase(object):
     def __init__(self):
         pass
 
-    @abc.abstractmethod
     @staticmethod
     def convert_ros_msg(msg):
         pass
 
-    @abc.abstractmethod
     @staticmethod
     def convert_to_ros_msg(obj):
         pass
