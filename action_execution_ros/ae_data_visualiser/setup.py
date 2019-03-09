81129c190e4a0bbf8f325be9239fedc520de5ee3
Henrik Schnor <hschnor@mailbox.org>
2019-03-09 12:59:11 +0100

    Remove another reference in ae_data_visualiser

--- action_execution_ros/ae_data_visualiser/setup.py
+++ action_execution_ros/ae_data_visualiser/setup.py
@@ -3,9 +3,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-dist_setup = generate_distutils_setup(
-    packages=['ae_data_visualiser'],
-    package_dir={'ae_data_visualiser': 'ros/src/ae_data_visualiser'}
-)
+dist_setup = generate_distutils_setup()
 
 setup(**dist_setup)
